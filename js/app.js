0aba8ad9fa9be73a74fdc6c010d063176ef2083e
rhondarobot <rrachellej@gmail.com>
2016-10-14 09:05:23 -0700

    trying to add constructor functions

--- js/app.js
+++ js/app.js
@@ -1,92 +1,124 @@
-/*Questions-- Set as pop-up/alert*/
-var Questions = function(){[
+$(function(){
+	alert('Pirates only! Enter at ye own risk matey');
+});
+
+var Questions = [{
 	one: 'Do ye like yer drinks strong?',
 	two: 'Do ye like it with a salty tang?',
 	three: 'Are you a lubber who likes it bitter?',
 	four: 'Would you like a bit of sweetness with yer poison?',
 	five: 'Are ye one for a fruity finish?'
-]};
+}]; //invoking function
+//trying to get questions to append to the DOM
+//$('.questions').append(this.one,this.two,this.three,this.four,this.five);
+console.log(Questions);
 
-
-/*Types of ingredients*/
-var Ingredients = function(mix){
-	this.mix = mix;
-};
-/*trying to figure which prototype this is*/
-	//Ingredients.prototype.ingrType = function(length){
-		// var drink = '';
-		// $.each(Ingredients.prototype.ingrType(function(i,ingredient){
-		// 	$('.drink-mix').append(results);
-		// })
-	var strong = function(){
-		Ingredients.call(this,['Glug of rum', 'slug of whisky', 'splash of gin']);
-	};
-	var salty = function(){
-		Ingredients.call(this,['Olive on a stick', 'salt-dusted rim', 'rasher of bacon']);	
-	}; 
-	var bitter = function(){ 
-		Ingredients.call(this,['Shake of bitters', 'splash of tonic', 'twist of lemon peel']);
-	};
-	var sweet = function(){
-		Ingredients.call(this,['Sugar cube', 'spoonful of honey', 'splash of cola']);
-	};
-	var fruity = function(){
-		Ingredients.call(this,['Slice of orange', 'dash of cassis', 'cherry on top']);
-	};
-
-/*Pantry houses ingredients--inheritance??*/
-var Pantry = function(Ingredients){
-	this.Ingredients = Ingredients;
+//trying to configure the pantry (in 2 spots right now)
+var Pantry = function(ingredients) {
+   this.ingredients = ingredients;
 };
 
-// *****example constructor function:*****
-// 	var Musician = function(sounds){
-// 		this.sounds = sounds;
-// 	};
+/*Setting the Pantry function to house the ingredients and set preferences--
+--preferences may need to go somewhere else*/
+// Pantry.prototype.preference = function(length) {
+//    var preference = '';
+//    for (var i=0; i<length; i++) {
+// 	preference += this.ingredients[i%this.ingredients.length]+' ';
+//     }
+//    console.log(preference);
+// };
 
-// 	var david = new Musician(['Twang','Thrumb','Bling']);
-// 	console.log(david.sounds);
-// *****end of constructor function example*****	
-/*Create constructor functions for:
-	questions
-	ingredients
-	pantry (which will contain available ingrs)
+var Ingredients = function(){
+	var Strong = ['Glug of rum', 'Slug of whisky', 'Splash of gin']; 	
+	var Salty = ['Olive on a stick', 'Salt-dusted rim', 'Rasher of bacon'];	 
+	var Bitter = ['Shake of bitters', 'Splash of tonic', 'Twist of lemon peel'];
+	var Sweet = ['Sugar cube', 'Spoonful of honey', 'Splash of cola'];
+	var Fruity = ['Slice of orange', 'Dash of cassis', 'Cherry on top'];
+};
+
+//var preference = newDrink(){
+	//
+//};
+// /*Pantry houses ingredients--inheritance??*/
+// var Pantry = function(Ingredients){
+// 	this.Ingredients = Ingredients;
+// };
+
+// // *****example constructor function:*****
+// // 	var Musician = function(sounds){
+// // 		this.sounds = sounds;
+// // 	};
+
+// // 	var david = new Musician(['Twang','Thrumb','Bling']);
+// // 	console.log(david.sounds);
+// // *****end of constructor function example*****	
+// /*Create constructor functions for:
+// 	questions
+// 	ingredients
+// 	pantry (which will contain available ingrs)
 
 
-Then use the constructors to create objects representing
-the bartenders questions/ingrs/pantry
+// Then use the constructors to create objects representing
+// the bartenders questions/ingrs/pantry
 
-Ask what style of a drink customer likes
-	//create a UI which will ask user for input for ea ?
-Create a Preferences{} that records their likes in the object
-*/
+// Ask what style of a drink customer likes
+// 	//create a UI which will ask user for input for ea ?
+// Create a Preferences{} that records their likes in the object
+// */
 
-/*Construct a drink object
-	create a constructor for a bartender object
-		>>method should take the preferences object and 
+// /*Construct a drink object
+// 	create a constructor for a bartender object
+// 		>>method should take the preferences object and 
 		
-		*****example of method*****
+// 		*****example of method*****
 		
-		var Musician = function(sounds){
-			this.sounds = sounds;
-		};
+// 		var Musician = function(sounds){
+// 			this.sounds = sounds;
+// 		};
 
-		Musician.prototype.solo = function(length) {
-			var solo = '';
-			for(var i=0; i<length; i++){
-				solo += this.sounds[i % this.sounds.length] + ' ';
-				}
-				console.log(solo);
-		};
+// 		Musician.prototype.solo = function(length) {
+// 			var solo = '';
+// 			for(var i=0; i<length; i++){
+// 				solo += this.sounds[i % this.sounds.length] + ' ';
+// 				}
+// 				console.log(solo);
+// 		};
 
-		var david = new Musician(['Twang','Thrumb','Bling']);
-		david.solo(8);
+// 		var david = new Musician(['Twang','Thrumb','Bling']);
+// 		david.solo(8);
 
-		*****end of method example*****
+// 		*****end of method example*****
 
-		>>construct a new drink object by fetching randomly
-		>>chosen ingredients from the pantry which match the user's preferences
-	create a bartender object 
-		>>add in logic so that when the user submits their preferences,
-		>>the drink is created and displayed in your UI
+// 		>>construct a new drink object by fetching randomly
+// 		>>chosen ingredients from the pantry which match the user's preferences
+// 	create a bartender object 
+// 		>>add in logic so that when the user submits their preferences,
+// 		>>the drink is created and displayed in your UI
+// */
+
+/*example from udemy guy
+function Person(firstname, lastname) {
+ 
+    console.log(this); //person{}
+    this.firstname = firstname;
+    this.lastname = lastname;
+    console.log('This function is invoked.');//This function is invoked
+    
+}
+
+Person.prototype.getFullName = function() {
+    return this.firstname + ' ' + this.lastname;   
+} //created a constructor function for the prototypes
+
+var john = new Person('John', 'Doe');
+console.log(john); //John Doe (getFullName)
+
+var jane = new Person('Jane', 'Doe');
+console.log(jane); //Jane Doe (getFullName)
+
+Person.prototype.getFormalFullName = function() {
+    return this.lastname + ', ' + this.firstname;   
+} 
+
+console.log(john.getFormalFullName()); //Doe, John  //Doe, Jane
 */
